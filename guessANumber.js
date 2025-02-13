9a011b6687c85ce382f9a64696bb094fea460814
AxelleBdd <axbaudinaud@gmail.com>
2025-02-13 16:01:19 +0100

    ajout des marges

--- guessANumber.js
+++ guessANumber.js
@@ -1,49 +1,64 @@
-let message = document.querySelector("h2");
-let tentative = document.querySelector("h3");
-let button = document.querySelector("#button");
-let count = 0;
-//Fonction qui demande au joueur 2 de fournir un nombre (avec un input):
-function chooseANumber(playerTwoInput) {
-    playerTwoInput = document.querySelector("#playerTwoInput").value;
-    return playerTwoInput
-}
 // Fonction qui demande au joueur 1 de fournir un nombre à deviner (playerOneChoice) compris entre 0 et 50 :
-function pickANumber(playerOneChoice) {
-    playerOneChoice = parseInt(prompt("J1 - Choisir un nombre entre 0 et 50:"));
+function pickANumber() {
+    let playerOneChoice = parseInt(prompt("J1 - Choisir un nombre entre 0 et 50:"));
     while (playerOneChoice < 0 || playerOneChoice > 50) {
         playerOneChoice = parseInt(prompt("J1 - Choisir un nombre entre 0 et 50:"));
     }
     return playerOneChoice;
 }
-// Comparaison de playerTwoChoice (givenNumber) à choosenNumber (playerOneChoice):
-function didIWin(playerTwoChoice, choosenNumber) {
-    if (playerTwoChoice == choosenNumber) {
+//Fonction qui demande au joueur 2 de fournir un nombre (avec un input):
+function chooseANumber() {
+    let playerTwoInput = document.querySelector("#playerTwoInput").value;
+    return playerTwoInput
+}
+// Comparaison de numberGiven (givenNumber) à numberToGuess (playerOneChoice):
+function didIWin(numberGiven, numberToGuess, margeMax, margeMin) {
+    if (numberGiven == numberToGuess) {
         return true;
     }
-    else if (playerTwoChoice > choosenNumber) {
+    else if (numberGiven > numberToGuess) {
         alert("Plus petit");
-        return false;
+        if (numberGiven<margeMax){
+            margeMax = numberGiven;
+            document.querySelector("label").innerHTML = `Deviner le nombre entre ${margeMin} et ${margeMax}:`;
+        } 
+        else {
+            alert("Le nombre est en dehors de l'intervalle.");
+        }
     }
-    else if (playerTwoChoice < choosenNumber) {
+    else if (numberGiven < numberToGuess) {
         alert("Plus grand");
-        return false;
+        if (numberGiven>margeMin){
+            margeMin = numberGiven;
+            document.querySelector("label").innerText = `Deviner le nombre entre ${margeMin} et ${margeMax} :`;
+        }
+        else {
+            alert("Le nombre est en dehors de l'intervalle.");
+        }
     }
-}
+    return false;  
+    }
 // Fonction d'appel des fonctions et d'enregistrement de la variable.
 function gamePlay() {
 //Appel de la fonction chooseANumber, pour que joueur 2 choisisse un nombre
-    givenNumber = chooseANumber();
+    let givenNumber = chooseANumber();
 // Appel de la fonction didIWin, pour que joueur 2 trouve le nombre.
-    result = didIWin(givenNumber, playerOneInput);
-    console.log(result);
+    let result = didIWin(givenNumber, playerOneInput,margeMax,margeMin);
     if (result === true){
-        message.innerText = "Bravo ! Vous avez deviné le nombre.🎉";
+        message.innerText = "Bravo ! Vous avez deviné le nombre🎉";
     }
     return
 }
+//Déclaration des variables
+let message = document.querySelector("h2");
+let tentative = document.querySelector("h3");
+let button = document.querySelector("#button");
+let count = 0;
+let margeMin = 0;
+let margeMax = 50;
 //Appel de la fonction pickANumber, pour que joueur 1 choisisse un nombre, sortie de gamePlay pour ne pas répéter 
 //la fonction à chaque validation de la valeur du joueur 2.
-playerOneInput = pickANumber();
+let playerOneInput = pickANumber();
 //Le boutton déclanche gamePlay.
 button.addEventListener("click", () => {
     gamePlay()
